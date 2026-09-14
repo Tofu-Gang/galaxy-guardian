7d639df186bba277457684df99754347c56444e9
Tofu Gang <tofugangsw@gmail.com>
2026-09-14 21:45:46 +0200

    50. Axis Input

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+const SPEED: int = 120
+const MOVE_LEFT_ACTION: StringName = "move_left"
+const MOVE_RIGHT_ACTION: StringName = "move_right"
+const MOVE_UP_ACTION: StringName = "move_up"
+const MOVE_DOWN_ACTION: StringName = "move_down"
+
 func _physics_process(_delta: float) -> void:
-	velocity = Vector2(160, 90)
+	var horizontal_direction: float = Input.get_axis(MOVE_LEFT_ACTION, MOVE_RIGHT_ACTION)
+	var vertical_direction: float = Input.get_axis(MOVE_UP_ACTION, MOVE_DOWN_ACTION)
+	var direction: Vector2 = Vector2(horizontal_direction, vertical_direction).normalized()
+	velocity = SPEED * direction
 	move_and_slide()
