2ddc719795a46bdd36eaedbe9a97035ab734892b
Tofu Gang <tofugangsw@gmail.com>
2026-09-14 23:08:43 +0200

    54. Animation Player

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,12 @@ const MOVE_RIGHT_ACTION: StringName = "move_right"
 const MOVE_UP_ACTION: StringName = "move_up"
 const MOVE_DOWN_ACTION: StringName = "move_down"
 
+# ship animations
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+const ANIMATION_DEFAULT: StringName = "default"
+const ANIMATION_UP: StringName = "up"
+const ANIMATION_DOWN: StringName = "down" 
+
 func _physics_process(_delta: float) -> void:
 	move()
 
@@ -19,6 +25,14 @@ func move() -> void:
 	var vertical_direction: float = Input.get_axis(MOVE_UP_ACTION, MOVE_DOWN_ACTION)
 	var direction: Vector2 = Vector2(horizontal_direction, vertical_direction).normalized()
 	velocity = SPEED * direction
+	
+	if vertical_direction < 0:
+		animation_player.play(ANIMATION_UP)
+	elif vertical_direction > 0:
+		animation_player.play(ANIMATION_DOWN)
+	else:
+		animation_player.play(ANIMATION_DEFAULT)
+		
 	# move the ship in the direction according to player input
 	move_and_slide()
 	# keep the ship inside the viewport
