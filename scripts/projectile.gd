bbaf875172b62913343420c6fba7a30cd4719904
Tofu Gang <tofugangsw@gmail.com>
2026-09-18 23:45:54 +0200

    63. VisibleOnScreenNotifier2D

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -3,8 +3,9 @@ extends Area2D
 # projectile speed; pixels per second
 @export var speed: float = 300.0
 
-func _ready() -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
 	global_position.x += speed * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	# remove the projectile from the game when it leaves the screen
+	queue_free()
